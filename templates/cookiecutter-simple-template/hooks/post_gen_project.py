b50a205e04a010ab6f7dffc6d572364e2bfbc722
YoungSoul <theyoungsoul@gmail.com>
2018-04-01 20:31:30 -0500

    initial content complete

---
+++ templates/cookiecutter-simple-template/hooks/post_gen_project.py
@@ -0,0 +1,7 @@
+
+def post_hooks():
+    print("********** POST GEN HOOK RUNNING ********")
+
+
+if __name__ == '__main__':
+    post_hooks()
---
+++ templates/cookiecutter-simple-template/hooks/pre_gen_project.py
@@ -0,0 +1,6 @@
+def pre_hooks():
+    print("********** PRE GEN HOOK RUNNING ********")
+
+
+if __name__ == '__main__':
+    pre_hooks()
